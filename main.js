d3d07dc1e45ac2d3903aa8f9b9bd927680e38833
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-05-13 23:46:38 +0100

    Display books from js

--- main.js
+++ main.js
@@ -24,3 +24,26 @@ function addBookToLibrary(event) {
   books.push(new Book(title, author, pages, read));
   event.preventDefault();
 }
+
+function displayBooks() {
+  books.forEach((e) => {
+    const main = document.querySelector("main");
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+    const name = document.createElement("h3");
+    name.textContent = e.name;
+    const author = document.createElement("h4");
+    author.textContent = e.author;
+    const pages = document.createElement("p");
+    pages.textContent = e.pages;
+    const read = document.createElement("p");
+    read.textContent = e.read;
+    card.appendChild(name);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    main.appendChild(card);
+  });
+}
+
+displayBooks();
