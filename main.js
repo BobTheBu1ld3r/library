94098de5a890feacc706bf723cbb20f8f47716ff
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-05-15 00:31:42 +0100

    Add ability to remove books

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ const formContainer = document.querySelector(".form-container");
 
 formContainer.addEventListener("click", handleAdd);
 
-const removeButton = document.querySelector(".book-card button");
-
 function handleAdd() {
   formContainer.classList.toggle("not-adding");
   document.querySelector("form").classList.toggle("not-adding");
@@ -54,8 +52,10 @@ function displayBook(book) {
   const card = document.createElement("div");
   card.classList.add("book-card");
   const cross = document.createElement("div");
+  cross.dataset.name = book.name;
   cross.innerHTML =
     '<button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" /></svg></button>';
+  cross.addEventListener("click", handleRemove);
   const name = document.createElement("h3");
   name.textContent = book.name;
   const author = document.createElement("h4");
@@ -70,6 +70,29 @@ function displayBook(book) {
   card.appendChild(pages);
   card.appendChild(read);
   main.insertBefore(card, document.querySelector(".form-container"));
+}
+
+function handleRemove(event) {
+  const main = document.querySelector("main");
+  const targetEl = event.target;
+  let card = targetEl;
+  while (card.tagName !== "DIV") {
+    card = card.parentNode;
+    console.log(card.tagName);
+  }
+  console.log(card);
+  const a = card.parentNode;
+  const name = card.dataset.name;
+  removeBookFromLibary(name);
+  main.removeChild(a);
+}
+
+function removeBookFromLibary(name) {
+  console.log(books);
+  books.forEach((book, index) => {
+    if (book.name === name) books.splice(index, 1);
+  });
+  console.log(books);
 }
 
 displayBooks();
