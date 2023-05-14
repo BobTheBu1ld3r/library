b2963e34e72bb86bbf79f4e91fbd03cacf5cfe42
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-05-14 23:34:39 +0100

    Add window-close svg

--- main.js
+++ main.js
@@ -52,7 +52,8 @@ function displayBook(book) {
   const card = document.createElement("div");
   card.classList.add("book-card");
   const cross = document.createElement("div");
-  cross.textContent = "\u274c";
+  cross.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" /></svg>';
   const name = document.createElement("h3");
   name.textContent = book.name;
   const author = document.createElement("h4");
